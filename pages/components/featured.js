966c900b6bfeac24d99dbe83b36d57f342f400ad
Byeongjun Moon <byeongjun@bjmoon.io>
2022-02-08 12:57:43 -0800

    web3

--- pages/components/featured.js
+++ pages/components/featured.js
@@ -3,10 +3,35 @@ import Web3 from 'web3';
 
 function Featured() {
     return (
-        <div className="w-full px-7 my-5">
-            <p className="font-medium text-xl">Start small with these top picks</p>
-            <div className="flex justify-between">
-                
+        <div className="w-full px-7">
+            <div className="-space-y-1 pb-5">
+                <p className="font-xs text-gray-600 ">February 5, 2022</p>
+                <p className="font-medium text-xl my-5">Today in All Communities</p>
+            </div>
+            <div className="carousel flex w-full">
+                <div className="flex justify-between gap-[24px]">
+                    <div className="bg-gray-200 h-48 md:h-96 min-w-[270px] w-full rounded-xl">
+
+                    </div>
+                    <div className="bg-gray-200 h-48 md:h-96 min-w-[270px] w-full rounded-xl">
+
+                    </div>
+                    <div className="bg-gray-200 h-48 md:h-96 min-w-[270px] w-full rounded-xl">
+
+                    </div>
+                    <div className="bg-gray-200 h-48 md:h-96 min-w-[270px] w-full rounded-xl">
+
+                    </div>
+                    <div className="bg-gray-200 h-48 md:h-96 min-w-[270px] w-full rounded-xl">
+
+                    </div>
+                    <div className="bg-gray-200 h-48 md:h-96 min-w-[270px] w-full rounded-xl">
+
+                    </div>
+                    <div className="bg-gray-200 h-48 md:h-96 min-w-[270px] w-full rounded-xl">
+
+                    </div>
+                </div>
             </div>
         </div>
     )
--- pages/components/navigation.js
+++ pages/components/navigation.js
@@ -35,25 +35,30 @@ function Navigation() {
         <div className="w-full px-7 my-5">
             <div className="flex justify-between">
                 <div className="flex items-center max-w-[200px] w-full">
-                    <div className="font-bold text-xl justify-start hover:cursor-pointer">Token</div>
+                    <div className="font-black text-xl justify-start hover:cursor-pointer">Token</div>
                 </div>
                 <div className="flex space-x-2 items-center justify-center w-full">
                     <div className="bg-blue-100 px-3 py-2 font-semibold text-blue-600 text-sm rounded-lg hover:cursor-pointer">Home</div>
                     <div className="flex items-center space-x-2 hover:bg-gray-100 px-3 py-2 font-medium text-black text-sm rounded-lg hover:cursor-pointer">
                         <div>Categories</div>
-                        <svg className="h-3 w-3 fill-gray-600 pt-0.5" xmlspace="preserve" width="512" height="298" shapeRendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 512 298.04"><path fill-rule="nonzero" d="M12.08 70.78c-16.17-16.24-16.09-42.54.15-58.7 16.25-16.17 42.54-16.09 58.71.15L256 197.76 441.06 12.23c16.17-16.24 42.46-16.32 58.71-.15 16.24 16.16 16.32 42.46.15 58.7L285.27 285.96c-16.24 16.17-42.54 16.09-58.7-.15L12.08 70.78z"/></svg>
+                        <svg className="h-3 w-3 fill-gray-600 pt-0.5" xmlspace="preserve" width="512" height="298" shapeRendering="geometricPrecision" textRendering="geometricPrecision" imageRendering="optimizeQuality" fillRule="evenodd" clipRule="evenodd" viewBox="0 0 512 298.04"><path fillRule="nonzero" d="M12.08 70.78c-16.17-16.24-16.09-42.54.15-58.7 16.25-16.17 42.54-16.09 58.71.15L256 197.76 441.06 12.23c16.17-16.24 42.46-16.32 58.71-.15 16.24 16.16 16.32 42.46.15 58.7L285.27 285.96c-16.24 16.17-42.54 16.09-58.7-.15L12.08 70.78z"/></svg>
                     </div>
                     <div className="hover:bg-gray-100 px-3 py-2 font-medium text-black text-sm rounded-lg hover:cursor-pointer">Communities</div>
                 </div>
                 <div className="flex space-x-4 items-center justify-end max-w-[200px] w-full">
-                    <div className="flex space-x-3 items-center">
-                        <BiSearch size={22} />
-                        <BiBell size={22} />
+                    <div className="flex items-center space-x-1">
+                        <div className="hover:bg-gray-100 rounded-lg p-2">
+                            <BiSearch size={22} />
+                        </div>
+                        <div className="hover:bg-gray-100 rounded-lg p-2">
+                            <BiBell size={22} />
+                        </div>
                     </div>
-                    <button className="px-3 py-2 font-semibold rounded-lg bg-blue-600 text-sm text-white hover:cursor-pointer" onClick={click}>Connect&nbsp;Wallet</button>
+                    <button className="px-3 py-2 font-semibold rounded-lg bg-gray-100 text-blue-600 text-sm hover:bg-blue-600 hover:text-white hover:cursor-pointer" onClick={click}>
+                        {account == null ? `Connect\u00A0Wallet` : `${account.slice(0, 6)}...${account.slice(-4)}`}
+                    </button>
                 </div>
             </div>
-            <p>{account}</p>
         </div>
     )
 }
